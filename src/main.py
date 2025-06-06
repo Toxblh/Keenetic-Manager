12f2786a5e5bfb54a0a46ea9cee29055ea38e0ec
Anton Palgunov <toxblh@gmail.com>
2025-06-06 01:16:46 +0100

    add flatpak vscode build + debug

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 # main.py
 import os
+if os.environ.get("DEBUG") == "1":
+    import debugpy
+    debugpy.listen(('127.0.0.1', 9002))
+    debugpy.wait_for_client()
+    debugpy.breakpoint()
+
+import os
 import sys
 from .router_manager_app import RouterManagerApplication
 
